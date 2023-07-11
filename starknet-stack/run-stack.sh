071d086ebead881a8331fd67f5c8262965f69305
d-roak <me+git@droak.sh>
2023-07-11 16:08:19 +0100

    use dockerfile branch for starknet_explorer

--- starknet-stack/run-stack.sh
+++ starknet-stack/run-stack.sh
@@ -7,14 +7,18 @@ cd "$(dirname "$0")"
 docker-compose up -d
 
 # Run LambdaClass explorer
+rm -rf starknet_explorer
 git clone https://github.com/lambdaclass/starknet_explorer || (cd starknet_explorer ; git pull)
 cd starknet_explorer
+git checkout dockerfile
 
-sed -ir 's/\.\/\.volumes\/postgres\//pg-/g' docker-compose.yml
-grep -qxF '  pg-data:' docker-compose.yml || echo '  pg-data:' >> docker-compose.yml
+# sed -ir 's/\.\/\.volumes\/postgres\//pg-/g' docker-compose.yml
+# grep -qxF '  pg-data:' docker-compose.yml || echo '  pg-data:' >> docker-compose.yml
 
 # the var needs to be exported
-export RPC_API_HOST=http://127.0.0.1:9944
-make setup run
-make run
+# export RPC_API_HOST=http://127.0.0.1:9944
+# make setup run
+# make run
+
+docker-compose up -d
 
