6b6ffda3c83bbc064b9b4b972627a16d0a3fe399
Fred Tupas <fred.tupas21@gmail.com>
2023-07-14 13:04:07 +0200

    feat: parametrize environment variables

--- starknet-stack/misc-apps/deploy.sh
+++ starknet-stack/misc-apps/deploy.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-export SHARINGAN_RPC_URL=${RPC_URL}
-export STARKNET_NETWORK=sharingan
-export SHARINGAN_ACCOUNT_ADDRESS=0x3
-export SHARINGAN_PRIVATE_KEY=0x00c1cf1490de1352865301bb8705143f3ef938f97fdf892f1090dcb5ac7bcd1d
+# Dynamically set the RPC, account address and private key based on the network
+export STARKNET_NETWORK=${STARKNET_NETWORK}
+export SN=$(echo "${STARKNET_NETWORK}" | tr '[:lower:]' '[:upper:]')
+export "${SN}_RPC_URL"=${RPC_URL}
+export "${SN}_ACCOUNT_ADDRESS"=0x3
+export "${SN}_PRIVATE_KEY"=0x00c1cf1490de1352865301bb8705143f3ef938f97fdf892f1090dcb5ac7bcd1d
 
 cd /app/kakarot && make deploy &
 cd /app/starksheet-monorepo/packages/starksheet-cairo && poetry run python deploy/starksheet.py
