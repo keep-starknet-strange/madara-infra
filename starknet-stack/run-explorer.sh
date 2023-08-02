1e22caf1607a40dc916bd6d6333097e90bf7dc7f
d-roak <me+git@droak.sh>
2023-08-02 09:54:26 +0100

    fix explorer version

--- starknet-stack/run-explorer.sh
+++ starknet-stack/run-explorer.sh
@@ -5,11 +5,11 @@ cd "$(dirname "$0")"
 # Run LambdaClass explorer
 rm -rf madara_explorer
 git clone https://github.com/lambdaclass/madara_explorer
+cd madara_explorer
+git checkout eb66776d241c48b233e5c333707076d0b766e980
 
 # TODO refactor this when the image is deployed
-cp explorer/docker-compose.yml madara_explorer/
+cp ../explorer/docker-compose.yml .
 
-cd madara_explorer
-
-docker-compose -f ../docker-compose.yml up -d
+docker-compose -f ../docker-compose.yml up madara -d
 docker-compose up -d
